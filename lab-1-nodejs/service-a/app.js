27b76e241d751777a82ca5d3eb0600f7f9c35cf7
Carlos Santana <csantana23@gmail.com>
2019-08-23 22:25:32 -0400

    added format span

--- lab-1-nodejs/service-a/app.js
+++ lab-1-nodejs/service-a/app.js
@@ -15,70 +15,70 @@ app.get('/', (req, res) => res.send(`Hello World! counter=${counter++}`))
 
 // Let's capture http error span
 app.get('/error', (req, res) => {
-  res.status(500).send('oh no some error 	(ノ ゜Д゜)ノ ︵ ┻━┻');
+    res.status(500).send('some error (ノ ゜Д゜)ノ ︵ ┻━┻');
 })
 
 // Using the span inside a route handler
 const greetings = require('./greetings');
-app.get('/greetings', greetings)
+app.get('/sayHello/:name', greetings)
 
-
+app.disable('etag');
 app.listen(port, () => console.log(`Service ${serviceName} listening on port ${port}!`))
 
 
 function initTracer(serviceName) {
-  const initJaegerTracer = require("jaeger-client").initTracerFromEnv;
+    const initJaegerTracer = require("jaeger-client").initTracerFromEnv;
 
-  // Sampler set to const 1 to capture every request, do not do this for production
-  const config = {
-    serviceName: serviceName,
-  };
-  // Only for DEV the sampler will report every span
-  config.sampler = { type: "const", param: 1 };
+    // Sampler set to const 1 to capture every request, do not do this for production
+    const config = {
+        serviceName: serviceName,
+    };
+    // Only for DEV the sampler will report every span
+    config.sampler = { type: "const", param: 1 };
 
-  return initJaegerTracer(config);
+    return initJaegerTracer(config);
 }
 
 function tracingMiddleWare(req, res, next) {
-  const url = require("url");
-  const wireCtx = tracer.extract(opentracing.FORMAT_HTTP_HEADERS, req.headers);
-  const pathname = url.parse(req.url).pathname;
-  // Creating our span with context from incoming request
-  const span = tracer.startSpan(pathname, { childOf: wireCtx });
-  // Use the log api to capture a log
-  span.log({ 'event': 'request_received' });
+    const url = require("url");
+    const wireCtx = tracer.extract(opentracing.FORMAT_HTTP_HEADERS, req.headers);
+    const pathname = url.parse(req.url).pathname;
+    // Creating our span with context from incoming request
+    const span = tracer.startSpan(pathname, { childOf: wireCtx });
+    // Use the log api to capture a log
+    span.log({ 'event': 'request_received' });
 
-  // Use the setTag api to capture standard span tags for http traces
-  span.setTag(opentracing.Tags.HTTP_METHOD, req.method);
-  span.setTag(opentracing.Tags.SPAN_KIND, opentracing.Tags.SPAN_KIND_RPC_SERVER);
-  span.setTag(opentracing.Tags.HTTP_URL, req.url);
+    // Use the setTag api to capture standard span tags for http traces
+    span.setTag(opentracing.Tags.HTTP_METHOD, req.method);
+    span.setTag(opentracing.Tags.SPAN_KIND, opentracing.Tags.SPAN_KIND_RPC_SERVER);
+    span.setTag(opentracing.Tags.HTTP_URL, req.url);
 
-  // include trace ID in headers so that we can debug slow requests we see in
-  // the browser by looking up the trace ID found in response headers
-  const responseHeaders = {};
-  tracer.inject(span, opentracing.FORMAT_HTTP_HEADERS, responseHeaders);
-  res.set(responseHeaders)
+    // include trace ID in headers so that we can debug slow requests we see in
+    // the browser by looking up the trace ID found in response headers
+    const responseHeaders = {};
+    tracer.inject(span, opentracing.FORMAT_HTTP_HEADERS, responseHeaders);
+    res.set(responseHeaders)
 
-  // add the span to the request object for any other handler to use the span
-  Object.assign(req, { span });
+    // add the span to the request object for any other handler to use the span
+    Object.assign(req, { span });
 
-  // finalize the span when the response is completed
-  const finishSpan = () => {
-    // Capture the status code
-    span.setTag(opentracing.Tags.HTTP_STATUS_CODE, res.statusCode);
-    if (res.statusCode >= 500) {
-      // If error then set the span to error
-      span.setTag(opentracing.Tags.ERROR, true);
-      // Force the span to be collected for http errors
-      span.setTag(opentracing.Tags.SAMPLING_PRIORITY, 1);
-      // Response should have meaning info to futher troubleshooting
-      span.log({ 'event': 'error', 'message': res.statusMessage });
+    // finalize the span when the response is completed
+    const finishSpan = () => {
+        // Capture the status code
+        span.setTag(opentracing.Tags.HTTP_STATUS_CODE, res.statusCode);
+        if (res.statusCode >= 500) {
+            // If error then set the span to error
+            span.setTag(opentracing.Tags.ERROR, true);
+            // Force the span to be collected for http errors
+            span.setTag(opentracing.Tags.SAMPLING_PRIORITY, 1);
+            // Response should have meaning info to futher troubleshooting
+            span.log({ 'event': 'error', 'message': res.statusMessage });
+        }
+        span.log({ 'event': 'request_end' });
+        span.finish();
     }
-    span.log({ 'event': 'request_end' });
-    span.finish();
-  }
-  res.on('finish', finishSpan);
+    res.on('finish', finishSpan);
 
-  console.log("request_received")
-  next()
+    console.log("request_received")
+    next()
 }
--- lab-1-nodejs/service-a/greetings.js
+++ lab-1-nodejs/service-a/greetings.js
@@ -2,26 +2,38 @@ const opentracing = require("opentracing");
 const tracer = opentracing.globalTracer();
 
 
-
-function greetings(req, res) {
+let counter = 1;
+function sayHello(req, res) {
     // You can re-use the parent span or create a child span
-    let span = ctx = tracer.startSpan("greetings_handler", { childOf: req.span })
+    let span = ctx = tracer.startSpan("say-hello", { childOf: req.span });
 
     // Parse the handler input
-    let name = req.query.name || "Carlos";
+    let name = req.params.name;
 
-    // show how to do a tag in the span
-    span.setTag("nameTag", name);
     // show how to do a log in the span
-    span.log({ event: 'greetings_handler', message: `this is a log message for name ${name}` });
+    span.log({ event: 'name', message: `this is a log message for name ${name}` });
     // show how to set a baggage item for context propagation (be careful is expensive)
-    span.setBaggageItem("nameBaggage", name);
+    span.setBaggageItem("myBaggage", name);
 
-    // simulate some processing time for 100ms to measure how long this span stook
+    // simulate a slow request every 3 requests
     setTimeout(() => {
-        span.finish()
-        res.send(`Greetings ${name}`)
-    }, 100);
+        let response = formatGreeting(name, span);
+        span.setTag("response", response);
+        span.finish();
+        res.send(response);
+    }, counter++ % 3 == 0 ? 100 : 0);
 }
 
-module.exports = greetings;
+function formatGreeting(name, parent) {
+    let span = tracer.startSpan("format-greeting", { childOf: parent })
+    let response = `Hello ${name}!`;
+    span.finish();
+    return response;
+}
+
+function formatGreetingRemote() {
+
+}
+
+
+module.exports = sayHello;
